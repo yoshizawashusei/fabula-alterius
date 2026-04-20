f8a0a670925b6446f538badd92994e0c630c8d81
yoshizawashusei3128 <yoshizawashusei@gmail.com>
2026-04-20 17:19:29 +0900

    chore: setup TailwindCSS

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,11 @@
 import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [
     react(),
+    tailwindcss(),
   ],
   server: {
     strictPort: true,
