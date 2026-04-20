36f2965f7b5e6255d88da869d411c42681659b1a
yoshizawashusei3128 <yoshizawashusei@gmail.com>
2026-04-20 09:17:55 +0900

    chore: setup playwright

---
+++ playwright.config.js
@@ -0,0 +1,23 @@
+import { defineConfig, devices } from '@playwright/test';
+
+export default defineConfig({
+  testDir: './tests/e2e',
+  fullyParallel: true,
+  reporter: 'html',
+  testMatch: './tests/e2e/**/*.tsx',
+  use: {
+    baseURL: 'http://localhost:5173',
+    trace: 'on-first-retry',
+  },
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    },
+  ],
+  webServer: {
+    command: 'npm run dev',
+    url: 'http://localhost:5173',
+    reuseExistingServer: !process.env.CI,
+  }
+});
