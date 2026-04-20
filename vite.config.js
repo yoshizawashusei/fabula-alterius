fb989def723fdbc6720824f797931cf2dbae6dca
yoshizawashusei3128 <yoshizawashusei@gmail.com>
2026-04-20 09:08:11 +0900

    chore: setup Vitest

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
   plugins: [],
@@ -8,4 +8,20 @@ export default defineConfig({
   preview: {
     strictPort: true,
   },
+  test: {
+    globals: true,
+    projects: [{
+      test: {
+        name: 'unit-test',
+        environment: 'node',
+        include: ['tests/unit/**/*.tsx']
+      }
+    },{
+      test: {
+        name: 'component-test',
+        environment: 'jsdom',
+        include: ['tests/component/**/*.tsx']
+      }
+    }]
+  }
 })
