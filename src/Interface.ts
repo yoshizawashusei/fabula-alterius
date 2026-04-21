ad9aec4ad58463ffc4af9bb4fb9d5c8fffaf64ca
yoshizawashusei3128 <yoshizawashusei@gmail.com>
2026-04-21 14:43:24 +0900

    feature: add BodyPart types (#4)

--- src/Interface.ts
+++ src/Interface.ts
@@ -1,1 +1,22 @@
 type TOPLevelData = { ID: String; Tags: Set<String> };
+
+type WithLore<T> = T & { Lore: String };
+
+type BodyPart =
+  | 'Head'
+  | 'Ear'
+  | 'Eye'
+  | 'Mouth'
+  | 'Neck'
+  | 'Torso'
+  | 'Back'
+  | 'LeftArm'
+  | 'LeftHand'
+  | 'RightArm'
+  | 'RightHand'
+  | 'Finger'
+  | 'Weist'
+  | 'LeftLeg'
+  | 'RightLeg'
+  | 'LeftFoot'
+  | 'RightFoot';
