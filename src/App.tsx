ee95a3f35866d0d66f5ab4ae6255c10ea2647676
yoshizawashusei3128 <yoshizawashusei@gmail.com>
2026-04-21 12:59:32 +0900

    feature: add Locations(Concept) (#3)
    現在地の表記と移動を実装した
    2つのcommitに分割するにはこの2機能の結合は強かった

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,23 @@
+import { create } from 'zustand';
+
+const useLocation = create((set) => ({
+  currentLocation: '初期地点',
+  moveTo: (to: String) => set({ currentLocation: to }),
+}));
+
 export default function App() {
-  return <div className="App">HELLO WORLD</div>;
+  const currentLocation = useLocation((state) => state.currentLocation);
+  const moveTo = useLocation((state) => state.moveTo);
+  return (
+    <>
+      <span>現在地:{currentLocation}</span>
+      <div
+        onClick={() => {
+          moveTo('2つめの地点');
+        }}
+      >
+        2つめの地点に移動
+      </div>
+    </>
+  );
 }
